fb89cea75ce8e0e653fe6a904f3114527c9b37e1
jeong-inn <jung1nn@naver.com>
2022-02-04 18:46:41 +0900

    feat : Smart Factory

---
+++ Kit_SmartFactory_python.py
@@ -0,0 +1,63 @@
+import time
+from machine import Pin, time_pulse_us
+from ETboard.lib.pin_define import *
+from ETboard.lib.OLED_U8G2 import *
+
+oled = oled_u8g2()
+RESET = Pin(D7)
+TRIG = Pin(D9)                               
+ECHO = Pin(D8)
+
+count = 0
+#temp_buffer[255] = {0, }
+pre_time = 0
+
+def oled_show(count) :
+    #str(count)
+    #global temp_buffer = "count : " + count
+    oled.setLine(1, "* Smart Factory *")
+    oled.setLine(2, count)
+    oled.setLine(3, "--------------")
+    oled.display()
+
+def setup():
+    RESET.init(Pin.OUT)
+    TRIG.init(Pin.OUT)                         
+    ECHO.init(Pin.IN)                         
+
+def loop():
+    global pre_time, count
+    TRIG.value(LOW)
+    ECHO.value(LOW)
+    time.sleep_ms(2)
+    TRIG.value(HIGH)
+    time.sleep_ms(10)
+    TRIG.value(LOW)
+    
+    duration = time_pulse_us(ECHO, HIGH)
+    distance = ((34 * duration) / 1000) / 2
+    
+    print(distance)
+    time.sleep(0.1)
+    
+    if( distance < 10 ) :
+        now_time = int(time.time()*1000.0)
+        if(now_time - pre_time > 500) :
+            count += 1
+            
+            print("count : ", count)
+            oled_show(count)
+            time.sleep(1)
+            
+            pre_time = now_time;
+            
+    if(RESET.value == LOW) :
+        print("reset count")
+        count = 0
+        oled_show(count)    
+ 
+    
+if __name__ == "__main__":
+    setup()
+    while True:
+        loop()
