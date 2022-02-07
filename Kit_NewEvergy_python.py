77df966dc7fb5cee5b8c57491bf80488ea5d64fb
jeong-inn <jung1nn@naver.com>
2022-02-07 11:24:33 +0900

    fix : DEC

--- Kit_NewEvergy_python.py
+++ Kit_NewEvergy_python.py
@@ -18,23 +18,23 @@ def setup_oled() :
     SCL.init(Pin.IN)
     
 def display_oled() :
-    solar_voltage_value = Pin(A3).value()
+    solar_voltage_value = Pin(A3).read()
     print("태양광 센서 : ", solar_voltage_value)
 
     text1[32] = "S : "
     value1[32]
-    str1 = str(solar_voltage_value * c_value, DEC)
+    str1 = str(solar_voltage_value * c_value)
     
     text1 = text1 + value1
     text1 = text1 + "V"
     list1 = list(str1)
-    windturbine_voltage_value = Pin(A5).value
+    windturbine_voltage_value = Pin(A5).read()
     print("풍력 센서 : ", windturbine_voltage_value)
     print("-------------------");
 
     text2[32] = "W : "
     value2[32]
-    str2 = Str(windturbine_voltage_value * c_value, DEC)
+    str2 = Str(windturbine_voltage_value * c_value)
     list2 = list(str2)
     text2 = text2 + value2
     text2 = text2 + "V"
