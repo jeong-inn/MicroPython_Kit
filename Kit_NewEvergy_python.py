2c00638def008d2e1485151bdb9e3f411f1a8788
jeong-inn <jung1nn@naver.com>
2022-02-07 10:49:47 +0900

    feat : New Energy

---
+++ Kit_NewEvergy_python.py
@@ -0,0 +1,50 @@
+import time
+from machine import Pin
+from ETboard.lib.OLED_U8G2 import *
+from ETboard.lib.pin_define import *
+
+c_value = 0.001221245421
+oled = oled_u8g2()
+
+def setup() :
+    pass
+
+def loop() :
+    display_oled()
+    time.sleep(1)
+
+def setup_oled() :
+    SDA.init(Pin.IN)
+    SCL.init(Pin.IN)
+    
+def display_oled() :
+    solar_voltage_value = Pin(A3).value()
+    print("태양광 센서 : ", solar_voltage_value)
+
+    text1[32] = "S : "
+    value1[32]
+    str1 = str(solar_voltage_value * c_value, DEC)
+    
+    text1 = text1 + value1
+    text1 = text1 + "V"
+    list1 = list(str1)
+    windturbine_voltage_value = Pin(A5).value
+    print("풍력 센서 : ", windturbine_voltage_value)
+    print("-------------------");
+
+    text2[32] = "W : "
+    value2[32]
+    str2 = Str(windturbine_voltage_value * c_value, DEC)
+    list2 = list(str2)
+    text2 = text2 + value2
+    text2 = text2 + "V"
+
+    oled.setLine(1, "* ECO Energy *")
+    oled.setLine(2, text1)
+    oled.setLine(3, text2)
+    oled.display()
+
+if __name__ == "__main__" :
+    setup()
+    while True :
+        loop()
