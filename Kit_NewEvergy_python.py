27f19b2ef227a9eff058f7cbb82346f48d05f011
jeong-inn <jung1nn@naver.com>
2022-02-07 16:15:58 +0900

    fix : ADC

--- Kit_NewEvergy_python.py
+++ Kit_NewEvergy_python.py
@@ -1,14 +1,21 @@
 import time
-from machine import Pin
+from machine import Pin, ADC
 from ETboard.lib.OLED_U8G2 import *
 from ETboard.lib.pin_define import *
 
-c_value = 0.001221245421
 oled = oled_u8g2()
+solar = ADC(Pin(A3))
+windturbine = ADC(Pin(A5))
+
+text1 = [0] * 31
+text2 = [0] * 31
 
 def setup() :
-    pass
-
+    #solar.init(Pin.IN)
+    #windturbine.init(Pin.IN)
+    solar.atten(ADC.ATTN_11DB)
+    windturbine.atten(ADC.ATTN_11DB) 
+    
 def loop() :
     display_oled()
     time.sleep(1)
@@ -18,27 +25,17 @@ def setup_oled() :
     SCL.init(Pin.IN)
     
 def display_oled() :
-    solar_voltage_value = Pin(A3).read()
-    print("태양광 센서 : ", solar_voltage_value)
+    solar_voltage_value = solar.read()
+    windturbine_voltage_value = windturbine.read()
 
-    text1[32] = "S : "
-    value1[32]
-    str1 = str(solar_voltage_value * c_value)
-    
-    text1 = text1 + value1
-    text1 = text1 + "V"
-    list1 = list(str1)
-    windturbine_voltage_value = Pin(A5).read()
+    print("태양광 센서 : ", solar_voltage_value)
     print("풍력 센서 : ", windturbine_voltage_value)
     print("-------------------");
 
-    text2[32] = "W : "
-    value2[32]
-    str2 = Str(windturbine_voltage_value * c_value)
-    list2 = list(str2)
-    text2 = text2 + value2
-    text2 = text2 + "V"
-
+    text1 = "s : %d V" %(solar_voltage_value)
+    text2 = "w : %d V" %(windturbine_voltage_value)
+    
+    oled.clear()
     oled.setLine(1, "* ECO Energy *")
     oled.setLine(2, text1)
     oled.setLine(3, text2)
