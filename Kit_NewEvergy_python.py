356a73b2bb137af437217c6c748d8a4d6aa80b23
jeong-inn <jung1nn@naver.com>
2022-02-08 13:58:15 +0900

    fix : Descriprion

--- Kit_NewEvergy_python.py
+++ Kit_NewEvergy_python.py
@@ -1,3 +1,13 @@
+# ******************************************************************************************
+# FileName     : NewEnergy_Basic_python
+# Description  : 빨강 LED 가 켜졌다 꺼졌다 반복
+# Author       : 이승찬
+# Created Date : 2021.08.19
+# Reference    :
+# Modified     :
+# ******************************************************************************************
+
+
 import time
 from machine import Pin, ADC
 from ETboard.lib.OLED_U8G2 import *
@@ -11,8 +21,6 @@ text1 = [0] * 31
 text2 = [0] * 31
 
 def setup() :
-    #solar.init(Pin.IN)
-    #windturbine.init(Pin.IN)
     solar.atten(ADC.ATTN_11DB)
     windturbine.atten(ADC.ATTN_11DB) 
     
@@ -25,8 +33,8 @@ def setup_oled() :
     SCL.init(Pin.IN)
     
 def display_oled() :
-    solar_voltage_value = solar.read()
-    windturbine_voltage_value = windturbine.read()
+    solar_voltage_value = solar.read()# 태양광 발전량 측정 센서
+    windturbine_voltage_value = windturbine.read() # 풍력 발전량 측정 센서
 
     print("태양광 센서 : ", solar_voltage_value)
     print("풍력 센서 : ", windturbine_voltage_value)
@@ -36,12 +44,18 @@ def display_oled() :
     text2 = "w : %d V" %(windturbine_voltage_value)
     
     oled.clear()
-    oled.setLine(1, "* ECO Energy *")
-    oled.setLine(2, text1)
-    oled.setLine(3, text2)
+    oled.setLine(1, "* ECO Energy *")# OLED 첫 번째 줄 : 시스템 이름
+    oled.setLine(2, text1) # OLED 두 번째 줄 : 태양광 발전량
+    oled.setLine(3, text2)# OLED 세 번째 줄 : 풍력 발전량
     oled.display()
 
 if __name__ == "__main__" :
     setup()
     while True :
         loop()
+
+#=========================================================================================
+#
+# (주)한국공학기술연구원 http://et.ketri.re.kr
+#
+#=========================================================================================
